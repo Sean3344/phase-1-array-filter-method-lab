fa4e88149136f8e942f76a6a17c8bc27ecb29aea
Sean Nekouei <sean.nekouei@gmail.com>
2022-12-18 20:43:57 -0700

    commit tree 2bff38de1a96e2de123cbeb8205ea2371090f7b0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+function findMatching(array, soughtName) { 
+    return array.filter(name => name.toUpperCase() === soughtName.toUpperCase())
+    
+
+
+}
+function fuzzyMatch(array, string) {
+    return array.filter(name => name.indexOf(string) === 0)
+}
+
+function matchName(array, string) {
+    return array.filter(Object => Object.name === string)
+    
+}
